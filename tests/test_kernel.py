20b1743af10b67974c66f74aec0122d85e5f21b4
Fergal Cotter <fbcotter90@gmail.com>
2018-05-01 12:46:41 +0100

    Updated cmake files to build ops

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -2,11 +2,18 @@ import tensorflow as tf
 import numpy as np
 import py3nvml
 
-def test_zeroout():
+import pytest
+import os
+
+LIB_BASE = os.path.join(os.path.dirname(__file__), '..', 'lib')
+
+def test_kernel():
     py3nvml.grab_gpus(0)
-    zero_out_module = tf.load_op_library('../bin/zero_out.so')
+    kernel_module = tf.load_op_library(os.path.join(LIB_BASE, 'kernel_example.so'))
     with tf.Session() as sess:
-        result = zero_out_module.zero_out([5, 4, 3, 2, 1])
-        np.testing.assert_array_equal(result.eval(), np.array([5,0,0,0,0]))
+        result = kernel_module.example([5, 4, 3, 2, 1])
+        np.testing.assert_array_equal(result.eval(), np.array([10, 8, 6, 4, 2]))
 
 
+if __name__ == '__main__':
+    pytest.main([__file__])
--- tests/test_zero_out.py
+++ tests/test_zero_out.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 import numpy as np
 import py3nvml
+import pytest
+import os
+
+LIB_BASE = os.path.join(os.path.dirname(__file__), '..', 'lib')
 
 def test_zeroout():
     py3nvml.grab_gpus(0)
-    zero_out_module = tf.load_op_library('../bin/zero_out.so')
+    zero_out_module = tf.load_op_library(os.path.join(LIB_BASE, 'zero_out.so'))
     with tf.Session() as sess:
         result = zero_out_module.zero_out([5, 4, 3, 2, 1])
         np.testing.assert_array_equal(result.eval(), np.array([5,0,0,0,0]))
 
+
+if __name__ == '__main__':
+    pytest.main([__file__])
